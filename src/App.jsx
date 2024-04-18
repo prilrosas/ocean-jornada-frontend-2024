0d1e30cfb84b8f9af1c55ba1f945a67069653f95
Priscila Lima <94015991+prilrosas@users.noreply.github.com>
2024-04-18 23:51:40 +0000

    criação do card com eslitilazação

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import './App.css'
 function App() {
   return (
     <>
-     Hello Word
+    <div className='card'>
+      <h2>Rick Sanches</h2>
+      <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'/>
+    </div>
     </>
   )
 }
